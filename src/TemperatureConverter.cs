94144b3346245c66e63dee49b8774b2c4cf5f87b
AtheerIbrahim <159605191+AtheerIbrahim@users.noreply.github.com>
2024-09-09 15:59:43 +0300

    level 1 added

---
+++ src/TemperatureConverter.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace TemperatureConverter
+{
+    public class Program
+    {
+        public static string TempConvert(int amount, string unit)
+        {
+            int converter;
+            string result;
+            if( unit.ToUpper() == "F" )
+            {
+                converter = (amount-32)*5/9;
+                result = $"{converter} C";
+                return result;
+            }
+            else
+            {
+                converter = amount*9/5+32;
+                result = $"{converter} F";
+                return result;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to Temperature Converter Example:");
+            Console.WriteLine(TempConvert(32, "f"));
+            Console.WriteLine(TempConvert(100, "c"));
+        }
+    }
+}
