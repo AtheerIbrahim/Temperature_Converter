49c5c10c77ec04088b53b7406558f381d544c51b
AtheerIbrahim <159605191+AtheerIbrahim@users.noreply.github.com>
2024-09-10 10:05:11 +0300

    level 2 added

--- src/TemperatureConverter.cs
+++ src/TemperatureConverter.cs
@@ -3,28 +3,27 @@ namespace TemperatureConverter
 {
     public class Program
     {
-        public static string TempConvert(int amount, string unit)
+        public static string TempConvert(float amount, string unit)
         {
-            int converter;
             string result;
             if( unit.ToUpper() == "F" )
             {
-                converter = (amount-32)*5/9;
-                result = $"{converter} C";
+                result = $"{Math.Round((amount-32)*5/9, 2)} C";
                 return result;
             }
             else
             {
-                converter = amount*9/5+32;
-                result = $"{converter} F";
+                result = $"{Math.Round(amount*9/5+32, 2)} F";
                 return result;
             }
         }
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Welcome to Temperature Converter Example:");
-            Console.WriteLine(TempConvert(32, "f"));
-            Console.WriteLine(TempConvert(100, "c"));
+            Console.WriteLine($"Welcome to Temperature Converter Example");
+            Console.WriteLine($"Enter a temperature and its unit (C or F):");
+            float value = Convert.ToSingle(Console.ReadLine());
+            string scale = Console.ReadLine() ?? ""; //avoiding nullable
+            Console.WriteLine($"Converted: {value} {scale} = {TempConvert(value, scale)}");
         }
     }
 }
